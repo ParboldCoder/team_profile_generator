a13f85551a270aee99a34d36b404d710411083b9
ParboldCoder <darren.a.buck@gmail.com>
2024-02-25 16:37:41 +0000

    rewrote and modified the index.js to a more condensed written file

--- challenge/starter/index.js
+++ challenge/starter/index.js
@@ -1,15 +1,47 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const { Manager, Engineer, Intern } = require("./lib");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+const team = [];
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+async function addEmployee(role) {
+  const questions = [
+    { type: "input", name: "name", message: `Input ${role}'s Name` },
+    { type: "input", name: "id", message: `Input ${role}'s ID Number` },
+    { type: "input", name: "email", message: `Input ${role}'s Email` },
+    { type: "input", name: "extra", message: `Input ${role}'s ${role === "Manager" ? "Office Number" : role === "Engineer" ? "GitHub Username" : "Current School"}` },
+  ];
 
+  const answers = await inquirer.prompt(questions);
+  const employee = new (role === "Manager" ? Manager : role === "Engineer" ? Engineer : Intern)(answers.name, answers.id, answers.email, answers.extra);
+  team.push(employee);
+  
+  await promptUser();
+}
+
+async function promptUser() {
+  const userChoice = await inquirer.prompt([
+    { type: "list", name: "menu", message: "What would you like to do?", choices: ["Add a Manager", "Add an Engineer", "Add an Intern", "Finish building the team"] },
+  ]);
+
+  if (userChoice.menu.startsWith("Add")) {
+    const role = userChoice.menu.split(" ").pop();
+    await addEmployee(role);
+  } else {
+    generateHTML();
+  }
+}
+
+async function generateHTML() {
+  const html = render(team);
+  fs.writeFileSync(outputPath, html);
+  console.log("HTML successfully generated!");
+}
+
+promptUser();
